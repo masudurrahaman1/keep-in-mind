a48375d3fa4d8bd559e161ab689a05a553f89e7e
masudurrahaman1 <masudurrahaman@gmail.com>
2026-04-27 02:30:22 +0530

    build: convert web app to android app via capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.keepinmind.app',
+  appName: 'Keep In Mind',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
